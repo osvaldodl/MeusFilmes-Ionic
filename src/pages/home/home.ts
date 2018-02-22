17f9ff61a2e289bc21aee560a8b2e7fbc45e05c3
marionaldocabral <marionaldocabral@hotmail.com>
2018-02-22 14:31:11 -0300

    correções de bug

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,6 +6,7 @@ import { FavoritosPage } from '../favoritos/favoritos';
 import { HistoricoPage } from '../historico/historico';
 import { AppPreferences } from '@ionic-native/app-preferences';
 import { ListaProvider } from '../../providers/lista/lista';
+import { ToastController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -15,25 +16,46 @@ export class HomePage {
 
   lista = []
   constructor(public navCtrl: NavController,
-    public platform:Platform, public prefs: AppPreferences, private listaProvider: ListaProvider) {
-      this.prefs.fetch(null,'lista').then(x => {
+    public platform:Platform,
+    public prefs: AppPreferences,
+    private listaProvider: ListaProvider,
+    private toasty: ToastController) {
+      this.platform.ready().then(() => this.prefs.fetch(null,'lista').then(x => {
         this.lista = x || [];
         console.log(x);
-        if(this.lista.length == 0){
-          this.carregaFilmes();
+        if(this.lista.length == 0){          
+            this.carregaFilmes();           
+        }        
+        else{
+          console.log("Nao e necessario baixar os dados");
         }
-      else{
-        console.log("Nao e necessario baixar os dados");
-      }
-      }).catch(erro => console.log("Nao foi possivel recuperar dados"));     
+      }).catch(erro => console.log("Nao foi possivel recuperar dados")));     
   }
   
-  carregaFilmes(){   
+  avisoToast(mensagem: string) {
+    let toast = this.toasty.create({
+      message: mensagem,
+      duration: 3000,
+      position: 'middle', 
+    });
+  
+    toast.onDidDismiss(() => {
+      console.log('Dismissed toast');
+    });
+  
+    toast.present();
+  }
+
+  carregaFilmes(){
     var y = [];
-    for(var i = 1; i<26; i++){
-      this.listaProvider.getFilmes(i).then(x => this.lista= y.concat(this.lista, x.results)).then(() => this.prefs.store(null,'lista', this.lista));
-    }
-    console.log("Dados baixados");
+    var cont = 0;
+    for(var i = 1; i < 26; i++){
+      this.listaProvider.getFilmes(i).then(x => this.lista= y.concat(this.lista, x.results)).then(() => {
+        this.prefs.store(null,'lista', this.lista);
+        cont++;
+        console.log("Dados baixados");
+      }).catch(erro => this.avisoToast("Falha ao baixar conteúdo. Verifique suas conexões de rede."));
+    }  
   }  
 
   toLista(event){
